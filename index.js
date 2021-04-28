7e0c39779006d4d200b0ff9a3a9ea8b92c722098
Max Mardany <Max@Maxs-MacBook-Air.local>
2021-04-27 20:00:42 -0400

    request for all data matches url: `http://localhost:1337/movies`

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express")
-
+const movies = require("./movies")
+const { response } = require("express")
 const app = express()
 
 app.get("/", (request, response) => {
-    return response.send("Movies")
+  return response.send("Movies")
+})
+
+app.get(
+  "/movies",
+  (getMovies = (request, response) => {
+    return response.send(movies)
+  })
+)
+
+app.listen(1400, () => {
+  console.log("Listening on port 1400...") //eslint-disable-line no-console
 })
