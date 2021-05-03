1d16aa13b873042d62d9673fe04608c3ece098c9
Max Mardany <Max@Maxs-MacBook-Air.local>
2021-05-02 22:34:44 -0400

    movies able to return by title

--- index.js
+++ index.js
@@ -14,6 +14,26 @@ app.get(
   })
 )
 
+app.get("/movies/:title", (request, response) => {
+  const movieTitle = movies.find((movie) => movie.title)
+  return response.send(movieTitle)
+  console.log(request, response)
+})
+
+app.get("/movies/:directors", (request, response) => {
+  const movieDirector = movies.find(
+    (movie) => movie.directors.includes(request.params.directors) 
+  )
+
+  // console.log(movies)
+  // console.log(movies[0])
+  console.log(request.params)
+
+  if (movieDirector.includes(request.params.directors))
+    console.log(request, response)
+  return response.send(movieDirector)
+})
+
 app.listen(1400, () => {
   console.log("Listening on port 1400...") //eslint-disable-line no-console
 })
